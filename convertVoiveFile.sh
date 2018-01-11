33d5242586f71c5d490bdec69a974596a59b8628
Jinhuyk Park <pjh08190819@naver.com>
2018-01-11 21:41:57 +0900

    test

--- convertVoiveFile.sh
+++ convertVoiveFile.sh
@@ -4,6 +4,10 @@
 
 export AudilFile=./audioFile
 
-sox --channels=2 --bits=16 --rate=44100 --encoding=signed-integer --endian=little $AudilFile/test.flac --channels=1 --bits=16 --rate=16000 $AudilFile/res.flac
+ffmpeg -i $AudilFile/test.mp4 $AudilFile/test.flac
 
-sox $AudilFile/res.flac $AudilFile/out.flac trim 0 3 : newfile : restart
+sox --channels=2 --bits=24 --rate=44100 --encoding=signed-integer --endian=little $AudilFile/test.flac --channels=1 --bits=16 --rate=16000 $AudilFile/res.flac
+
+sox -V3 $AudilFile/res.flac $AudilFile/out.flac silence 1 0.50 0.1% 1 0.3 0.1% : newfile : restart
+
+#sox $AudilFile/res.flac $AudilFile/out.flac trim 0 3 : newfile : restart
