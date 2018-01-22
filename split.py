d3f1194e1b6d7e08b50270331eb52084fac1fa86
튼튼 <yw@teunteun-ui-MacBook-Pro.local>
2018-01-22 17:17:15 +0900

    보기 편하게 정리

--- split.py
+++ split.py
@@ -2,60 +2,83 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence, detect_nonsilent
 import datetime
 
-sound_file = AudioSegment.from_file("./audioFile/test.mp4", format="mp4")
-nonsilent_ranges = detect_nonsilent(sound_file, min_silence_len=500, silence_thresh=-50)
-audio_chunks = list()
+def split_sound_file(path, format):
+    """Split sound file && Return nonsilent ranges."""
 
-keep_silence = 100
-for start_i, end_i in nonsilent_ranges:
-    start_i = max(0, start_i - keep_silence)
-    end_i += keep_silence
+    sound_file = AudioSegment.from_file(path, format=format)
+    nonsilent_ranges = detect_nonsilent(sound_file, min_silence_len=500, silence_thresh=-50)
+    audio_chunks = list()
 
-    audio_chunks.append(sound_file[start_i:end_i])
+    keep_silence = 100
+    for start_i, end_i in nonsilent_ranges:
+        start_i = max(0, start_i - keep_silence)
+        end_i += keep_silence
 
-for i, chunk in enumerate(audio_chunks):
-    out_file = "./audioFile_python/test{0}.flac".format(i)
-    print ("exporting", out_file, " - non silent- time : ", nonsilent_ranges[i][0],"~",nonsilent_ranges[i][1]) 
-    chunk.export(out_file, format="flac")
+        audio_chunks.append(sound_file[start_i:end_i])
 
-srt_file = open("test.srt", 'wt')
-srt_lines = list()
-index = 1
+    for i, chunk in enumerate(audio_chunks):
+        out_file = "./audioFile_python/test{0}.flac".format(i)
+        print ("exporting", out_file, " - non silent- time : ",
+            nonsilent_ranges[i][0], "~", nonsilent_ranges[i][1])
+        chunk.export(out_file, format="flac")
 
-while True:
-    file_name = "result" + str(index) + ".txt"
-    try:
-        src_file = open("./text/"+file_name, "rt")
-    except:
-        print("End File")
-        break
+    return nonsilent_ranges
 
-    lines = ''
-    for line in src_file:
-        lines += line
+def list_from_file():
+    """list from file."""
 
-    srt_lines.append(lines)
-    index += 1
-    src_file.close()
+    srt_lines = list()
+    index = 1
 
-block_num = 1
-init_time = datetime.datetime(100,1,1,0,0,)
-for line in srt_lines:
-    nonsilent_milliseconds = nonsilent_ranges[(block_num-1)]
+    while True:
+        file_name = "result" + str(index) + ".txt"
+        try:
+            src_file = open("./text/"+file_name, "rt")
+        except:
+            print("End File")
+            break
 
-    start_time = str((init_time + datetime.timedelta(0,milliseconds=nonsilent_milliseconds[0])).time())
-    end_time = str((init_time + datetime.timedelta(0,milliseconds=nonsilent_milliseconds[1])).time())
+        lines = ''
+        for line in src_file:
+            lines += line
 
-    srt_file.write(str(block_num))
-    srt_file.write("\n")
-    srt_file.write(start_time[:-3])
-    srt_file.write(" --> ")
-    srt_file.write(end_time[:-3])
-    srt_file.write("\n")
-    srt_file.write(line)
-    srt_file.write("\n")
-    srt_file.write("\n")
+        srt_lines.append(lines)
+        index += 1
+        src_file.close()
+    return srt_lines
 
-    block_num+=1
+def create_srt_file(srt_lines, nonsilent_ranges):
+    """create_srt_file."""
 
-srt_file.close()
+    srt_file = open("test.srt", 'wt')
+    block_num = 1
+    init_time = datetime.datetime(100, 1, 1, 0, 0,)
+
+    for line in srt_lines:
+        nonsilent_milliseconds = nonsilent_ranges[(block_num-1)]
+
+        start_time = str((init_time + datetime.timedelta(0,milliseconds=nonsilent_milliseconds[0])).time())
+        end_time = str((init_time + datetime.timedelta(0,milliseconds=nonsilent_milliseconds[1])).time())
+
+        srt_file.write(str(block_num))
+        srt_file.write("\n")
+        srt_file.write(start_time[:-3])
+        srt_file.write(" --> ")
+        srt_file.write(end_time[:-3])
+        srt_file.write("\n")
+        srt_file.write(line)
+        srt_file.write("\n")
+        srt_file.write("\n")
+
+        block_num+=1
+
+    srt_file.close()
+
+def main():
+    """main"""
+    nonsilent_ranges = split_sound_file("./audioFile/test.mp4", "mp4")
+    srt_lines = list_from_file()
+    create_srt_file(srt_lines, nonsilent_ranges)
+
+if __name__ == "__main__":
+    main()
