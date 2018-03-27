5937fbd463e6dff3ec0cb01a50158257cde6e394
HaeChang <gail5135@gmail.com>
2018-03-27 17:18:57 +0900

    ListTest

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,17 @@
 window.onload = function(){
-   document.getElementById("videoB").onclick = function(){
+   document.getElementById("UploadVideo").onclick = function(){
      // makeRequestVideo('http://210.204.165.166:4088/test')
-     makeRequestVideo('http://localhost:8080/test')
+     requestSendVideo('http://localhost:8080/test')
+   }
+   document.getElementById("RequestVideoList").onclick = function(){
+     requestVideoList('http://localhost:8080/videoList');
+   }
+   document.getElementById("VideoTime").onclick = function() {
+     requestVideoTime('http://localhost:8080/Time');
    }
 }
-function makeRequestVideo(url){
+
+function requestSendVideo(url){
   var form =$('form')[0];
   var formData = new FormData(form);
   var response;
@@ -25,4 +32,51 @@ function makeRequestVideo(url){
       alert("error");
     }
   })
+}
+
+function requestVideoList(url) {
+  var VideoList; 
+  
+  $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "json",
+
+    success:function(resp){
+      VideoList = resp;
+      alert("success");
+      alert(videoList);
+      return VideoList;
+    },
+    error:function(){
+      alert("error");
+    }
+  })
+}
+
+function requestVideoTime(url) {
+  var startTime;
+  var endTime;
+  $.ajax({
+
+
+    success:function(argument) {
+      // body...
+    }
+    error:function(argument){
+      // body...
+    }
+  })
+}
+
+function showBooks(ajax) {
+  var data = JSON.parse(ajax.responseText);
+    for (var i = 0; i < data.books.length; i++) {
+      var startLi = document.createElement("li");
+      var endLi = document.createElement("li");
+      startLi.innerHTML = "start: " + data.times[i].start;
+      endLi.innerHTML = "end: " + data.times[i].end;
+      document.body.appendChild(startLi);
+      document.body.appendChild(endLi);
+    }
 }
