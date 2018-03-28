d5ad801b4a7643be850873f09bb0ff38528d7c8b
HaeChang <gail5135@gmail.com>
2018-03-28 16:36:27 +0900

    video_play

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+   requestReceiveVideo('http://localhost:8080/test2');
    document.getElementById("UploadVideo").onclick = function(){
      // makeRequestVideo('http://210.204.165.166:4088/test')
      requestSendVideo('http://localhost:8080/test')
@@ -38,28 +39,100 @@ function requestSendVideo(url){
 }
 
 function requestReceiveVideo(url) {
-  var data;
+$.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
+      // check for conditions and support for blob / arraybuffer response type
+      if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob)))))
+      {
+          return {
+              // create new XMLHttpRequest
+              send: function(headers, callback){
+      // setup all variables
+                  var xhr = new XMLHttpRequest(),
+      url = options.url,
+      type = options.type,
+      async = options.async || true,
+      // blob or arraybuffer. Default is blob
+      dataType = options.responseType || "blob",
+      data = options.data || null,
+      username = options.username || null,
+      password = options.password || null;
+            
+                  xhr.addEventListener('load', function(){
+        var data = {};
+        data[options.dataType] = xhr.response;
+        // make callback and send data
+        callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+                  });
+   
+                  xhr.open(type, url, async, username, password);
+          
+      // setup custom headers
+      for (var i in headers ) {
+        xhr.setRequestHeader(i, headers[i] );
+      }
+          
+                  xhr.responseType = dataType;
+                  xhr.send(data);
+              },
+              abort: function(){
+                  jqXHR.abort();
+              }
+          };
+      }
+  });
+
   $.ajax({
     type: "GET",
     url: url,
-    dataType: "json",
+    dataType: "binary",
+    processData: false,
     async: false,
 
     success:function(resp){
       alert("success");
-      alert(resp);
+
+      var myVideo = document.createElement("video");
+      myVideo.width = 600;
+      myVideo.height = 500; 
+      myVideo.src = URL.createObjectURL(resp);
+      myVideo.autoplay = true;
+      myVideo.controls = true;
+      document.getElementById("myVideo").appendChild(myVideo);
       console.log(resp);
-      //document.getElementById("myVideo").src = resp;
     },
     error:function(){
       alert("error");
     }
   })
+
+
+  // $.ajax({
+  //   type: "GET",
+  //   url: url,
+  //   dataType: "blob",
+    
+  //   success:function(resp){
+  //     alert(resp);
+  //     var reader = new FileReader();
+  //     reader.readAsDataURL(resp);
+      
+  //     reader.onload = function () {
+  //       var myVideo = document.createElement("video");
+  //       myVideo.width = 400;
+  //       myVideo.height = 300;
+       
+  //       myVideo.src = reader.result;
+  //       document.getElementById("myVideo").appendChild(myVideo);
+  //     }
+  //       alert("success");
+  //   },
+  //   error:function(){
+  //     alert("error");
+  //   }
+  // })
 }
 
 function requestVideoList(url) {
-  var VideoList; 
-  
   $.ajax({
     type: "GET",
     url: url,
