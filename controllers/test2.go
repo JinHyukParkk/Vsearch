efbffd5734a6170aaba4c3a6b38f9d86b0663557
Jinhuyk Park <pjh08190819@naver.com>
2018-03-28 15:36:07 +0900

    test

--- controllers/test2.go
+++ controllers/test2.go
@@ -2,18 +2,16 @@ package controllers
 
 import (
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/labstack/echo"
 )
 
 func Test2(c echo.Context) error {
 	log.Println("hello test2")
-	f, err := os.Open("./audioFile/test.mp4")
-	if err != nil {
-		return err
-	}
+	// f, err := os.Open("./audioFile/test.mp4")
+	// if err != nil {
+	// 	return err
+	// }
 
-	return c.Stream(http.StatusOK, "video/mp4", f)
+	return c.File("./audioFile/test.mp4")
 }
