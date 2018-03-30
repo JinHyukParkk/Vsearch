7e375cb5caef92c65c5e08d1a9901d06592e9adf
Jinhuyk Park <pjh08190819@naver.com>
2018-03-31 01:12:25 +0900

    test

--- shell/convertVoiveFile.sh
+++ shell/convertVoiveFile.sh
@@ -11,7 +11,7 @@ sox --channels=2 --bits=24 --rate=44100 --encoding=signed-integer --endian=littl
 
 python3 $modulePath/main.py
 
-# rm $resPath/*
+rm $resPath/*
 
 # testList=`ls ./audioFile_python/test*`
 
@@ -20,7 +20,7 @@ python3 $modulePath/main.py
 # for ((i=0;i<$fs_count;i++))
 # do
 #   sox --channels=2 --bits=16 --rate=44100 --encoding=signed-integer --endian=little $resPath/test$i.flac --channels=1 --bits=16 --rate=16000 $resPath/result$i.flac
-#   rm $resPath/test$i.flac
+  # rm $resPath/result$i.flac
 # done
 
 #sox -V3 $AudilFile/res.flac $AudilFile/out.flac silence 1 0.50 0.1% 1 0.3 0.1% : newfile : restart
