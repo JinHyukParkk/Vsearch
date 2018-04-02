7bb9b543d3f4aca88f86774007f9b52199662660
Jinhuyk Park <pjh08190819@naver.com>
2018-04-02 12:10:13 +0900

    editing test2 response-json

--- controllers/test2.go
+++ controllers/test2.go
@@ -4,25 +4,31 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/JinHyukParkk/CapstoneProject/GoogleAPI"
+	"github.com/JinHyukParkk/CapstoneProject/models"
 	"github.com/labstack/echo"
 )
 
 func Test2(c echo.Context) error {
 	log.Println("hello test2")
-	// f, err := os.Open("./audioFile/test.mp4")
-	// check(err)
 
+	url := "https://storage.googleapis.com/testvideostore/test.mp4"
+	res_url := models.Response1{
+		Url: url,
+	}
 	// Read file to storage
 
-	log.Println("Read File to Storage")
-	f, err := googleApi.StorageRead("test.mp4")
-	check(err)
-	log.Println("Finish Read")
-
-	return c.Blob(http.StatusOK, "video/mp4", f)
-	// return c.Stream(http.StatusOK, "video/mp4", f)
+	return c.JSON(http.StatusOK, res_url)
 	// return c.File("./audioFile/test.mp4")
-
-	// response 로 json(cloud 서버 포함) or  Blob 보낼지 선택.
 }
+
+// f, err := os.Open("./audioFile/test.mp4")
+// check(err)
+// return c.Stream(http.StatusOK, "video/mp4", f)
+
+// log.Println("Read File to Storage")
+// f, err := googleApi.StorageRead("test.mp4")
+// check(err)
+// log.Println("Finish Read")
+// return c.Blob(http.StatusOK, "video/mp4", f)
+
+// response 로 json(cloud 서버 포함) or  Blob 보낼지 선택.
---
+++ models/Response1.go
@@ -0,0 +1,5 @@
+package models
+
+type Response1 struct {
+	Url string `json:"Url"`
+}
