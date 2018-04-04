e7789d30681d58862ac31a0142e7029a95adb8d3
hyh4827 <hyh4827@naver.com>
2018-04-04 14:08:49 +0900

    180404

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 window.onload = function(){
-   // requestReceiveVideo('http://localhost:8080/test2');
+
+
    document.getElementById("UploadVideo").onclick = function(){
      // makeRequestVideo('http://210.204.165.166:4088/test')
      requestSendVideo('http://localhost:8080/test')
@@ -9,6 +10,9 @@ window.onload = function(){
      requestVideoTime('http://localhost:8080/test1')
    }
 
+   document.getElementById("PlayVideo").onclick = function() {
+     requestReceiveVideo('http://localhost:8080/test2')
+   }
 
   /*document.getElementById("RequestVideoList").onclick = function(){
      requestVideoList('http://localhost:8080/videoList');
@@ -39,62 +43,68 @@ function requestSendVideo(url){
 }
 
 function requestReceiveVideo(url) {
-$.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
-      // check for conditions and support for blob / arraybuffer response type
-      if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob)))))
-      {
-          return {
-              // create new XMLHttpRequest
-              send: function(headers, callback){
-      // setup all variables
-                  var xhr = new XMLHttpRequest(),
-      url = options.url,
-      type = options.type,
-      async = options.async || true,
-      // blob or arraybuffer. Default is blob
-      dataType = options.responseType || "blob",
-      data = options.data || null,
-      username = options.username || null,
-      password = options.password || null;
+  /*
+  <<< dub용 코드. >>>
+  <<< 혹시 몰라서 주석처리 해둠. >>>
 
-                  xhr.addEventListener('load', function(){
-        var data = {};
-        data[options.dataType] = xhr.response;
-        // make callback and send data
-        callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
-                  });
-
-                  xhr.open(type, url, async, username, password);
-
-      // setup custom headers
-      for (var i in headers ) {
-        xhr.setRequestHeader(i, headers[i] );
-      }
-
-                  xhr.responseType = dataType;
-                  xhr.send(data);
-              },
-              abort: function(){
-                  jqXHR.abort();
-              }
-          };
-      }
-  });
-
+  $.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
+        // check for conditions and support for blob / arraybuffer response type
+        if (window.FormData && ((options.dataType && (options.dataType == 'binary')) || (options.data && ((window.ArrayBuffer && options.data instanceof ArrayBuffer) || (window.Blob && options.data instanceof Blob)))))
+        {
+            return {
+                // create new XMLHttpRequest
+                send: function(headers, callback){
+        // setup all variables
+                    var xhr = new XMLHttpRequest(),
+        url = options.url,
+        type = options.type,
+        async = options.async || true,
+        // blob or arraybuffer. Default is blob
+        dataType = options.responseType || "blob",
+        data = options.data || null,
+        username = options.username || null,
+        password = options.password || null;
+              
+                    xhr.addEventListener('load', function(){
+          var data = {};
+          data[options.dataType] = xhr.response;
+          // make callback and send data
+          callback(xhr.status, xhr.statusText, data, xhr.getAllResponseHeaders());
+                    });
+     
+                    xhr.open(type, url, async, username, password);
+            
+        // setup custom headers
+        for (var i in headers ) {
+          xhr.setRequestHeader(i, headers[i] );
+        }
+            
+                    xhr.responseType = dataType;
+                    xhr.send(data);
+                },
+                abort: function(){
+                    jqXHR.abort();
+                }
+            };
+        }
+    });
+  */
   $.ajax({
     type: "GET",
     url: url,
-    dataType: "binary",
+    dataType: "JSON",
     processData: false,
     async: false,
 
     success:function(resp){
       alert("success");
-
-      var myVideo = document.createElement("video");
-      myVideo.width = 600;
-      myVideo.height = 500;
-      myVideo.src = URL.createObjectURL(resp);
+      
+      var myVideo = document.createElement("iframe");
+      //myVideo.width = 600;
+      //myVideo.height = 500; 
+      myVideo.id = "VideoPlayer";
+      myVideo.className = "embed-responsive-item";
+      myVideo.src = resp.Url;
       myVideo.autoplay = true;
       myVideo.controls = true;
       document.getElementById("myVideo").appendChild(myVideo);
@@ -106,30 +116,6 @@ $.ajaxTransport("+binary", function(options, originalOptions, jqXHR){
   })
 
 
-  // $.ajax({
-  //   type: "GET",
-  //   url: url,
-  //   dataType: "blob",
-
-  //   success:function(resp){
-  //     alert(resp);
-  //     var reader = new FileReader();
-  //     reader.readAsDataURL(resp);
-
-  //     reader.onload = function () {
-  //       var myVideo = document.createElement("video");
-  //       myVideo.width = 400;
-  //       myVideo.height = 300;
-
-  //       myVideo.src = reader.result;
-  //       document.getElementById("myVideo").appendChild(myVideo);
-  //     }
-  //       alert("success");
-  //   },
-  //   error:function(){
-  //     alert("error");
-  //   }
-  // })
 }
 
 function requestVideoList(url) {
@@ -157,16 +143,41 @@ function requestVideoTime(url) {
 
     success:function(resp){
       alert("success");
-      alert(resp.times[0].start_time);
 
+      document.getElementById('TimeList').innerHTML = "";
+      
+      //영상 중간재생 버튼 생성.
       for(var i = 0; i < resp.times.length; i++){
-        var li = document.createElement("li");
-        li.innerHTML = "start: " + resp.times[i].start_time + "~ end: " + resp.times[i].end_time;
-        document.getElementById("TimeList").appendChild(li);
+        var btn = document.createElement("input");
+        btn.type = "button";
+        btn.className = "btn btn-default";
+        btn.id = "th"+i;
+        btn.value = resp.times[i].start_time;
+        console.log(resp.times[i].start_time);
+        btn.placeholder = "start: " + resp.times[i].start_time + "~ end: " + resp.times[i].end_time;
+
+        document.getElementById("TimeList").appendChild(btn);
+
+        var br = document.createElement("br");
+        document.getElementById("TimeList").appendChild(br);
       }
+
+      //버튼클릭시 해당 영상 시간으로 이동.
+      var buttons = document.getElementById("TimeList").childNodes;
+      for(var j = 0; j < buttons.length; j++){
+        buttons[j].onclick = function(){
+        document.getElementById("VideoPlayer").currentTime = this.value;
+      
+        }
+         
+      }
+     
+
     },
     error:function(){
       alert("error");
     }
   })
 }
+
+
