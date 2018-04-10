ebb046c40e959a2b7ba615537a0cb87edddea2e6
Jinhuyk Park <pjh08190819@naver.com>
2018-04-11 00:15:47 +0900

    test

--- models/KeywordModel.go
+++ models/KeywordModel.go
@@ -4,7 +4,6 @@ type KeywordModel struct {
 	Times []Time `json:"times"`
 }
 type Time struct {
-	File_name  string `json:"file_name"`
 	Start_Time string `json:"start_time"`
 	End_Time   string `json:"end_time"`
 }
