f133a69cc5d1e0aafbe4d92228e6dcefcf4762c1
Ywook <cywibo@gmail.com>
2018-05-08 18:41:46 +0900

    update

--- controllers/videoUpload.go
+++ controllers/videoUpload.go
@@ -46,7 +46,7 @@ func VideoUpload(c echo.Context) error {
 	log.Println("====Finish Upload CloudStorage====")
 
 	log.Println("====Upload dataStroage====")
-	googleApi.DataStorageUpload(files[0].Filename, files[0].Filename)
+	googleApi.DataStorageUpload(files[0].Filename, files[0].Filename, "temp_title")
 	log.Println("====Finish Upload dataStorage====")
 
 	s := strings.Split(files[0].Filename, ".")
--- models/ImageListModel.go
+++ models/ImageListModel.go
@@ -1,6 +1,7 @@
 package models
 
 type ImageVideo struct {
+	Title      string `datastore:"title"`
 	Image_name string `datastore:"imagename"`
 	Video_name string `datastore:"videoname"`
 }
